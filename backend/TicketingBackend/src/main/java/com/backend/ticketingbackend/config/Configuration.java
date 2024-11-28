43d34a3f58f19929b64789b794bdd364460644a6
SeniBiseka <senibiseka2@gmail.com>
2024-11-28 22:14:24 +0530

    config start

---
+++ backend/TicketingBackend/src/main/java/com/backend/ticketingbackend/config/Configuration.java
@@ -0,0 +1,10 @@
+package com.backend.ticketingbackend.config;
+
+public class Configuration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    
+}
