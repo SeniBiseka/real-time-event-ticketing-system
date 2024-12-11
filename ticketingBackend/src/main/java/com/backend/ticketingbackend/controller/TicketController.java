b4fb46af028d10ebdb01200d0f49e86d43ea504a
SeniBiseka <senibiseka2@gmail.com>
2024-12-11 10:49:31 +0530

    springboot step1
    created the controller package and ticketcontroller class.

---
+++ ticketingBackend/src/main/java/com/backend/ticketingbackend/controller/TicketController.java
@@ -0,0 +1,15 @@
+package com.backend.ticketingbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketController {
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Spring Boot is working!";
+    }
+}
+
